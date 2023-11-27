7e1fedd4bd279e2d9898c0218e583fcb1434f616
Byron Kontou <byronkontou@gmail.com>
2023-11-27 00:44:44 -0500

    added map

---
+++ Assets/Scripts/GameMap.cs
@@ -0,0 +1,121 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class GameMap : MonoBehaviour
+{
+    public Transform pc;
+    public RawImage game_map_image;
+    public UnityEngine.UI.Image pc_position_image;
+    public RawImage blackout_image_prefab;
+    public Canvas canvas_object;
+
+    public int map_divisions = 10;
+    public float map_size = 400f;
+    public float map_update_polling_timer = 0.6f;
+
+    private bool[,] visited_areas;
+    private RawImage[,] blackout_images;
+    private float _polling_timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        visited_areas = new bool[map_divisions, map_divisions];
+        blackout_images = new RawImage[map_divisions, map_divisions];
+
+        for (int i = 0; i < map_divisions; i++)
+        {
+            for (int j = 0; j < map_divisions; j++)
+            {
+                visited_areas[i,j] = false;
+                
+                RawImage blackout_obj = Instantiate<RawImage>(blackout_image_prefab);
+                blackout_obj.transform.SetParent(game_map_image.transform);
+                Vector2 blackout_obj_loc = index_to_map_position(new Vector2Int(i,j));
+                blackout_obj.rectTransform.sizeDelta = new Vector2(map_size / map_divisions, map_size / map_divisions);
+                blackout_obj.rectTransform.anchoredPosition = blackout_obj_loc;
+                //blackout_obj.rectTransform.rect.Set(blackout_obj_loc.x, blackout_obj_loc.y, map_size / map_divisions, map_size / map_divisions);
+                print(blackout_obj.rectTransform.rect);
+
+                blackout_images[i,j] = blackout_obj;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_polling_timer >= map_update_polling_timer)
+        {
+            update_map();
+        }
+        else
+        {
+            _polling_timer += Time.deltaTime;
+        }
+    }
+
+    private void update_map()
+    {
+        Vector2Int pc_position_index = world_position_to_index(pc.position);
+        visited_areas[pc_position_index.x, pc_position_index.y] = true;
+        update_map_image();
+    }
+
+    private void update_map_image()
+    {
+        Vector2 pc_map_position = world_position_to_map_position(pc.position);
+        pc_position_image.rectTransform.anchoredPosition = pc_map_position;
+
+        for (int i = 0; i < map_divisions; i++)
+        {
+            for(int j = 0; j < map_divisions; j++)
+            {
+                if (visited_areas[i,j]) {
+                    blackout_images[i, j].color = Vector4.zero;
+                }
+            }
+        }
+    }
+
+    private Vector2Int world_position_to_index(Vector3 position)
+    {
+        int array_index;
+
+        int position_x = Mathf.FloorToInt((position.x / map_size) * map_divisions);
+        int position_z = Mathf.FloorToInt((position.z / map_size) * map_divisions);
+
+        return new Vector2Int((int) Mathf.Clamp(position_x, 0, map_divisions - 1), (int)Mathf.Clamp(position_z, 0, map_divisions - 1));
+
+        //array_index = (map_divisions - 1) * position_x + map_divisions + position_z;
+
+        //return (int) Mathf.Clamp(array_index, 0, map_divisions * map_divisions);
+    }
+
+    private Vector2 world_position_to_map_position(Vector3 position)
+    {
+        Vector2 map_position = Vector2.zero;
+
+        float position_x_relative = (position.x / map_size);
+        float position_z_relative = (position.z / map_size);
+
+        Rect image_rect = game_map_image.rectTransform.rect;
+
+        map_position = new Vector2(0.5f * image_rect.width - position_z_relative * image_rect.width, position_x_relative * image_rect.height - 0.5f * image_rect.height);
+
+        return map_position;
+    }
+
+    private Vector2 index_to_map_position(Vector2Int index)
+    {
+        Vector2 map_position = Vector2.zero;
+
+        float map_pos_x = ((float)index.x / map_divisions) * map_size + 0.5f * (map_size / map_divisions);
+        float map_pos_z = ((float)index.y / map_divisions) * map_size + 0.5f * (map_size / map_divisions);
+
+        map_position = world_position_to_map_position(new Vector3(map_pos_x, 0f, map_pos_z));
+
+        return map_position;
+    }
+}
